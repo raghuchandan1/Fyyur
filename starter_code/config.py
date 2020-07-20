5f74092fc785cd4fab3036efb18643adbd748c44
Raghu Chandan <33779280+Raghu-Chandan@users.noreply.github.com>
2020-07-20 08:57:53 +0530

    feat: Edited the create venue form to include new fields fix: Crash on creating a new venue

--- starter_code/config.py
+++ starter_code/config.py
@@ -10,4 +10,4 @@ DEBUG = True
 
 
 # DONE IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres@localhost:5432/postgres'
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:Postgres@1@localhost:5432/postgres'
